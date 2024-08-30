74e49d7914cdf36abd1eddd3bd73519c549aaee3
Farzaliyev <nureddin.fa@gmail.com>
2024-08-30 13:47:01 +0400

    login context

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,23 +2,24 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./components/home/Home"
 import { Toaster } from "react-hot-toast"
 import { useAuth } from "./hooks/useAuth"
-import { useEffect } from "react"
+import { useEffect  } from "react"
+import { isLoggedInContext } from "./contexts/isLoggedInContext"
 
 function App() {
   const {auth, isLoggedIn, isLoading} = useAuth()
-
   useEffect(() => {auth(); console.log(isLoggedIn)}, [])
 
   if(isLoading !== true){
     return (
       <>
       <Toaster />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" Component={Home} />
-        </Routes>
-      </BrowserRouter>
-      <p>{isLoggedIn === true ? 'logged in' : 'NOT logged in'}</p>
+      <isLoggedInContext.Provider value={isLoggedIn}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" Component={Home} />
+          </Routes>
+        </BrowserRouter>
+      </isLoggedInContext.Provider>
       </>
     )
   }else{
--- client/src/components/home/Home.tsx
+++ client/src/components/home/Home.tsx
@@ -1,15 +1,25 @@
 import Login from '../auth/Login'
 import Signup from '../auth/Signup'
 import Logout from '../auth/Logout'
+import { isLoggedInContext } from '../../contexts/isLoggedInContext'
+import { useContext } from 'react'
 
 function Home() {
-    return (
-        <div>
-            <Login />
-            <Signup />
+    const isLoggedIn = useContext(isLoggedInContext)
+
+    if(isLoggedIn === true){
+        return(
             <Logout />
-        </div>
-    )
+        )
+    }else{
+        return (
+            <div>
+                <Login />
+                <Signup />
+            </div>
+        )
+    }
+
 }
 
 export default Home
