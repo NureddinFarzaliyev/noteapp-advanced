85462d9b2dd4631cd120bbfe883034a8cf48aa94
Farzaliyev <nureddin.fa@gmail.com>
2024-09-11 17:08:43 +0400

    ui fix

--- client/src/components/auth/Login.tsx
+++ client/src/components/auth/Login.tsx
@@ -10,7 +10,7 @@ function Login() {
             <form className="text-white flex flex-col gap-5 items-start">
                 <h1 className="mt-10 text-xl">Login</h1>
                 <input className="bg-transparent border-b-2 border-b-cyan-900 focus:border-b-cyan-400 transition-all py-1" type="text" name="username" placeholder="Username" onChange={(e) => handleChange(e)} />
-                <input className="bg-transparent border-b-2 border-b-cyan-900 focus:border-b-cyan-400 transition-all py-1" type="text" name="password" placeholder="Password" onChange={(e) => handleChange(e)} />
+                <input className="bg-transparent border-b-2 border-b-cyan-900 focus:border-b-cyan-400 transition-all py-1" type="password" name="password" placeholder="Password" onChange={(e) => handleChange(e)} />
                 <button onClick={(e) => {login(changeData, e)}} className="bg-cyan-700 px-10 py-1 w-40 rounded shadow-md hover:bg-cyan-900 transition-all">{isLoading ? 'Loading...' : 'Login'}</button>
             </form>
         </div>
--- client/src/components/home/NotLoggedIn.tsx
+++ client/src/components/home/NotLoggedIn.tsx
@@ -3,15 +3,15 @@ import Signup from "../auth/Signup"
 
 function NotLoggedIn() {
     return (
-        <div className="bg-gray-900 h-dvh w-dvw p-10 flex justify-between">
+        <div className="bg-gray-900 md:h-dvh w-dvw p-10 md:flex justify-between">
             <div className="text-white w-[65%]">
                 <h1 className="text-5xl">NoteAPP</h1>
-                <p className="w-[50%] my-5">This is a MERN-Stack Note-taking Application which lets you create folder structure and save your notes in it. You can also customize the app with the colors you like.</p>
+                <p className="md:w-[50%] my-5">This is a MERN-Stack Note-taking Application which lets you create folder structure and save your notes in it. You can also customize the app with the colors you like.</p>
                 <a href="https://www.github.com/nureddinfarzaliyev/noteapp-advanced" className="opacity-70 hover:opacity-100 transition-all">
                     Made by <u>Nureddin Farzaliyev</u> Source code is available on <u>GitHub</u>.
                 </a>
             </div>
-            <div className="w-[35%]">
+            <div className="md:w-[35%]">
                 <Login />
                 <hr className="mt-10 opacity-20" />
                 <Signup />
